7a0b429f3dd5de5b30463022cbd6ffba5ec752db
LetPritom <pritom.royjess@gmail.com>
2025-11-25 03:13:57 +0600

    added scure my pass and DB name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors =require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = 3000;
-
 app.use(cors())
 app.use(express.json())
 
@@ -12,13 +12,10 @@ app.get('/' , (req , res) => {
     res.send('mongol a')
 })
 
-// study-mate
-// R1VTHb2NCpC4d1u9
 
 
 
-
-const uri = "mongodb+srv://study-mate:R1VTHb2NCpC4d1u9@cluster0.ouqgsfd.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ouqgsfd.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
